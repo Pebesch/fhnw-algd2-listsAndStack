6ed547fa09dc1395292878ded939d7ee05841feb
Peter Schmucki <peter.schmucki@valion.ch>
2020-06-06 18:21:54 +0200

    Implemented SinglyLinkedList

--- src/list/SinglyLinkedList.java
+++ src/list/SinglyLinkedList.java
@@ -1,23 +1,101 @@
 package list;
 
+import java.util.NoSuchElementException;
+
 public class SinglyLinkedList<E> {
     private int size;
+    private Element head;
 
     // API
-    public SinglyLinkedList() { }
-    public SinglyLinkedList(SinglyLinkedList<E> orig) { }
-    void InsertFirst(E e) { }
-    void insertAfter(E e, int index) { }
-    void removeFirst() { }
-    void remove(int index) { }
-    void removeAll() { }
-    E getFirst() { }
-    E get(int index) { }
-    int size() { }
-    public String toString() {}
+    public SinglyLinkedList() {
+        size = 0;
+        head = null;
+    }
+
+    public SinglyLinkedList(SinglyLinkedList<E> orig) {
+        // Push last element first from orig
+        for(int i = orig.size() - 1; i > 0; i--) {
+            insertFirst(orig.get(i));
+        }
+    }
+
+    void insertFirst(E e) {
+        Element element = new Element(e);
+        element.next = head;
+        head = element;
+        size++;
+    }
+
+    void insertAfter(E e, int index) {
+        if(index > size - 1) throw new IndexOutOfBoundsException();
+        Element current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        Element newElement = new Element(e);
+        newElement.next = current.next;
+        current.next = newElement;
+        size++;
+    }
+
+    void removeFirst() {
+        if (size > 0) {
+            head = head.next;
+            size--;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    void remove(int index) {
+        if(index > size - 1) throw new IndexOutOfBoundsException();
+        if(index != 0) {
+            Element current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        } else {
+            head = head.next;
+        }
+        size--;
+    }
+
+    void removeAll() {
+        head = null;
+        size = 0;
+    }
+
+    E getFirst() {
+        if (size == 0) throw new NoSuchElementException();
+        return (E)head.data;
+    }
+
+    E get(int index) {
+        if(index > size - 1) throw new IndexOutOfBoundsException();
+        Element current = head;
+        for(int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return (E)current.data;
+    }
+
+    int size() {
+        return size;
+    }
+
+    public String toString() {
+        String result = "";
+        Element<E> r = head;
+        while (r != null){
+            result += (r.data.toString() + "  ");
+            r = r.next;
+        }
+        return result;
+    }
 
     private static class Element<E> {
-        private E data;
+        E data;
         Element next;
 
         public Element(E data) {
